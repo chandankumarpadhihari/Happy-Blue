4d408a5754f781c4e89edc1da070d049e3dfcf99
Mr. Chandan Kumar <143110610+chandankumarpadhihari@users.noreply.github.com>
2026-07-28 16:53:31 +0530

    Add new product: Laptop Cooling Pad

--- js/products.js
+++ js/products.js
@@ -45,5 +45,25 @@ const PRODUCTS = [
       "Face ID and enhanced privacy protections."
     ],
     buyLink: "https://link.amazon/B0bkRwFFS"
+  },
+
+  {
+    id: 3,
+    title: "Laptop Cooling Pad",
+    category: "Electronics",
+    image: "https://m.media-amazon.com/images/I/61ti1QvYWZL._SL1500_.jpg",
+    price: 1599,
+    mrp: 4499,
+    rating: 4.5,
+    reviews: 458,
+    badge: "",
+    features: [
+      "Ergonomic design: The ZEB-NS4000 Pro rotatable laptop stand, with its foldable, ergonomic design and durable metal body, is perfect for remote work and workcations.",
+      "Optimum cooling: To combat the heat from the laptop, this stand comes with a type-C powered detachable magnetic fan that can boast 66CFM airflow for adequate cooling.",
+      "Portability: This stand is easily portable and can withstand a maximum weight of 5 kg, capable of holding a laptop of up to 43.18 cm.",
+      "Anti-rust: The anti-rust property of the ZEB-NS4000 Pro laptop stand will allow you to use it for the longest time without any damage.",
+      "360° rotatable: The laptop stand comes with 360° rotation to ease the strain from long-duration working."
+    ],
+    buyLink: "https://www.amazon.in/dp/B0DB8H4JWR?social_share=cm_sw_r_cso_cp_apin_dp_J4V724H81VBFD64NQF7N_1&linkCode=ll2&tag=happyblue050-21&linkId=70682ef041780d37ef88624dacd6938a&ref_=as_li_ss_tl"
   }
 ];
